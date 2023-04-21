11a991857b1f3d7cfcdf037c21fffd72b2c94386
Apsan <np03cs4a220109@heraldcollege.edu.np>
2023-04-21 15:33:37 +0545

    q7

--- Tutorial 9/students.sql
+++ Tutorial 9/students.sql
@@ -48,3 +48,6 @@ SELECT * FROM students WHERE gpa BETWEEN 3.00 AND 3.50;
 SELECT * FROM students WHERE major_subject = 'Computer Science';
 
 SELECT * FROM students WHERE gpa > (SELECT AVG(gpa) FROM students);
+
+SELECT * FROM students WHERE gpa BETWEEN (SELECT MIN(gpa) FROM students) AND (SELECT MAX(gpa) FROM students);
+
